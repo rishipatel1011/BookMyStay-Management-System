8c7b52fc41a3a768dff7fa8196bb25a3effe742b
Rishi <rishihansaliya07@gmail.com>
2026-03-20 16:35:59 +0530

    UC1: Foundation - Room Entity & Manual Verification

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,9 +1,11 @@
-public static void main(String[] args) {
-    System.out.println("--- Welcome to BookMyStay App ---");
+public class HotelBookingApp {
+    public static void main(String[] args) {
+        System.out.println("--- Welcome to Book My Stay App ---");
 
-    // FIX: Initialize the object here
-    Room room1 = new Room(101, "Deluxe");
+        // UC1: Manual Verification
+        Room room1 = new Room(101, "Deluxe");
 
-    System.out.println("System Check: Initializing Room Data...");
-    System.out.println(room1); // This will now work!
+        System.out.println("System Check: Initializing Room Data...");
+        System.out.println(room1);
+    }
 }
